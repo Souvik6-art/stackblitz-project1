eac4d7116c232eac70f73fe84908fe1a415a9564
Souvik6-art <sm6.souvik@gmail.com>
2026-07-19 01:59:45 +0530

    completed a basic project on changing of page color by clicking on the colour

--- Souvik_stackblitz1.js
+++ Souvik_stackblitz1.js
@@ -1,8 +1,50 @@
-const buttons =document.querySelectorAll('.button')
+ const buttons =document.querySelectorAll('.button')
 //console.log(buttons);
 const body= document.querySelector("body")
-
+//const canvas =document.querySelector("canvas")
 buttons.forEach(function(button){
   console.log(button);
+button.addEventListener('click',function(e){
+  console.log(e);
+console.log(e.target);
+/*
+if(e.target.id== 'grey'){
+  body.style.backgroundColor =e.target.id;
+ // canvas.style.backgroundColor=e.target.id
+}
+else if(e.target.id== "green"){
+  body.style.backgroundColor= e.target.id;
+}
+else if(e.target.id== "blue"){
+  body.style.backgroundColor=e.target.id;
+}
+else if(e.target.id== "yellow"){
+body.style.backgroundColor=e.target.id;
+}*/
+
+
+switch (e.target.id) {
+  case 'grey':
+    body.style.backgroundColor = e.target.id;
+        break;
+
+  case 'green':
+    body.style.backgroundColor = e.target.id;
+    break;
+
+  case 'blue':
+    body.style.backgroundColor = e.target.id;
+    break;
+
+  case 'yellow':
+    body.style.backgroundColor = e.target.id;
+    break;
+
+  default:
+    
+    break;
+}
+
 })
-console.log("Hello Souvik!");
+})
+
