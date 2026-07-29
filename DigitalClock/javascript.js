3723b7b1d1ec963efdeeea6c1fa4941888d0340c
Souvik6-art <sm6.souvik@gmail.com>
2026-07-30 00:42:41 +0530

    added the  date also to show

--- DigitalClock/javascript.js
+++ DigitalClock/javascript.js
@@ -5,3 +5,9 @@ setInterval(function(){
 
 clock.innerHTML =date.toLocaleTimeString();
 },1000) ;
+
+
+const date= document.getElementById('Date');
+let date1=new Date();
+//date.innerHTML= date1.toLocaleDateString();
+date.innerHTML=date1.toDateString();
